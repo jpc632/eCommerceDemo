4b67df80705f68589b843a463106d600ac79392f
John Paul Casey <jpc632@uowmail.edu.au>
2021-08-28 20:08:05 +1000

    Add models

---
+++ Shop.Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderReference { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
