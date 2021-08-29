7ca651f633e08f72f23291f72fd0a0af519ef73f
John Paul Casey <jpc632@uowmail.edu.au>
2021-08-29 16:01:04 +1000

    Add WebUI Project

---
+++ Shop.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Commands.Products.Interfaces;
+using Shop.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IGetProduct _getProductService;
+        private readonly IGetProducts _getProductsService;
+        private readonly ICreateProduct _createProductService;
+        private readonly IDeleteProduct _deleteProductService;
+        private readonly IUpdateProduct _updateProductService;
+
+        public AdminController(
+            IGetProduct getProductService,
+            IGetProducts getProductsService,
+            ICreateProduct createProductService,
+            IDeleteProduct deleteProductService,
+            IUpdateProduct updateProductService)
+        {
+            _getProductService = getProductService;
+            _getProductsService = getProductsService;
+            _createProductService = createProductService;
+            _deleteProductService = deleteProductService;
+            _updateProductService = updateProductService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id) => Ok(_getProductService.Do(id));
+
+        [HttpGet]
+        //public IActionResult GetProducts() => Ok(_getProductsService.Do());
+        public IEnumerable<ProductViewModel> GetProducts()
+        {
+            var res = _getProductsService.Do();
+            return res;
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductViewModel product) => Ok(_createProductService.Do(product));
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id) => Ok(_deleteProductService.Do(id));
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(ProductViewModel product) => Ok(_updateProductService.Do(product));
+    }
+}
