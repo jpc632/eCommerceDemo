980c5a4d8b1a834828e172d4fa128c46524eb7b5
John Paul Casey <jpc632@uowmail.edu.au>
2021-08-29 16:01:14 +1000

    Add Application DI

---
+++ Shop.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shop.Application.Commands.Products;
+using Shop.Application.Commands.Products.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IGetProduct, GetProduct>();
+            services.AddScoped<IGetProducts, GetProducts>();
+            services.AddScoped<ICreateProduct, CreateProduct>();
+            services.AddScoped<IDeleteProduct, DeleteProduct>();
+            services.AddScoped<IUpdateProduct, UpdateProduct>();
+        }
+    }
+}
--- Shop.Application/ViewModels/ProductViewModel.cs
+++ Shop.Application/ViewModels/ProductViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace Shop.UI.ViewModels
+namespace Shop.Application.ViewModels
 {
     public class ProductViewModel
     {
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+        [JsonPropertyName("value")]
         public decimal Value { get; set; }
     }
 }
