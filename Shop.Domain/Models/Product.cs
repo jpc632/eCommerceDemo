bfc38411878043d31f832d485fa029b21b3deda5
jpcasey <jpcasey@episoft.com.au>
2021-09-05 14:13:18 +1000

    Add Admin CRUD

--- Shop.Domain/Models/Product.cs
+++ Shop.Domain/Models/Product.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Shop.Domain.Models
 {
     public class Product
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+
+        [JsonPropertyName("value")]
         public decimal Value { get; set; }
 
         public ICollection<Stock> Stock { get; set; }
